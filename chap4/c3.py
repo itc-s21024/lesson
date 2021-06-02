5cd3d24163650b6cc993e99efa31dbc1be1985e4
yasumura kanta <s21024@std.it-college.ac.jp>
2021-06-02 08:12:42 +0000

    chap4

---
+++ chap4/c3.py
@@ -0,0 +1,5 @@
+def add_it(a,b,c,x=0,y=0):
+    return a + b + c + x + y
+
+result = add_it(1,2,3)
+print(result)
---
+++ chap4/c4.py
@@ -0,0 +1,7 @@
+def f(n):
+    return n // 2
+
+def f2(n):
+    return n * 4
+
+print(f2(f1(4)))
